fa803d364639fe1f48ed1c70803b3c9dfda1e9ae
Dima <47077994+mirrerror@users.noreply.github.com>
2023-09-24 18:04:49 +0300

    Delete src directory

--- src/main/java/md/mirrerror/entities/StudyField.java
+++
@@ -1,9 +0,0 @@
-package md.mirrerror.entities;
-
-public enum StudyField {
-    MECHANICAL_ENGINEERING, SOFTWARE_ENGINEERING, FOOD_TECHNOLOGY, URBANISM_ARCHITECTURE, VETERINARY_MEDICINE;
-
-    public static StudyField match(String s) {
-        return StudyField.valueOf(s.toUpperCase());
-    }
-}
